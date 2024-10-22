72d43e415996751c57265457f8351e60957d5d2f
Yash Patel <109963122+PatelYash7@users.noreply.github.com>
2024-10-22 23:56:15 +0530

    Added the Header section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Header } from "@/components/header";
+
 export default function Home() {
   return (
-    <div className="flex justify-center py-8">
-      This is Yash Patel Portfolio..
+    <div className="flex justify-center py-8  flex-col">
+      <Header/>
     </div>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export const Header = () => {
+  return (
+    <header className="my-4">
+      <Image
+        src={"https://avatars.githubusercontent.com/u/109963122?v=4"}
+        width={80}
+        height={80}
+        className="rounded-lg"
+        alt="profileimage"
+      ></Image>
+      <div>
+
+      <h1 className="text-4xl font-bold py-4">Yash Patel</h1>
+      <h2 className=" text-lg text-justify text-gray-400">
+        I'm a Full Stack Engineer with experience in building
+        <span className="text-white font-bold"> Web applications </span> end-to-end. I have a strong background in
+        both <span className="text-white font-bold"> Frontend </span>  and <span className="text-white font-bold"> Backend </span>  development with <span className="text-white font-bold"> DevOps </span>, ensuring smooth and
+        efficient deployments. I enjoy transforming ideas into Reality.
+      </h2>
+      </div>
+    </header>
+  );
+};
