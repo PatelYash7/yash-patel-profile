0777b649973cccb4748230109d7eeacc03e464e7
Yash Patel <109963122+PatelYash7@users.noreply.github.com>
2024-10-23 18:47:23 +0530

    Fixes the lint error.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -14,7 +14,7 @@ export const Header = () => {
 
       <h1 className="text-4xl font-bold py-4">Yash Patel</h1>
       <h2 className=" text-lg text-justify text-gray-400">
-        I'm a Full Stack Engineer with experience in building
+        I&apos;m a Full Stack Engineer with experience in building
         <span className="text-white font-bold"> Web applications </span> end-to-end. I have a strong background in
         both <span className="text-white font-bold"> Frontend </span>  and <span className="text-white font-bold"> Backend </span>  development with <span className="text-white font-bold"> DevOps </span>, ensuring smooth and
         efficient deployments. I enjoy transforming ideas into Reality.
