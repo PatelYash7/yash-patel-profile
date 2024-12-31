155772c1bf8d3a531c72825d14aaa733c1b88af2
Yash Patel <109963122+PatelYash7@users.noreply.github.com>
2024-12-31 17:27:22 +0530

    Added Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 export const metadata: Metadata = {
   title: "Yash Patel",
   description: "Full Stack Engineer",
@@ -15,6 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`antialiased bg-[#0f0f0f]`}>{children}
         <Analytics/>
+        <SpeedInsights/>
       </body>
     </html>
   );
